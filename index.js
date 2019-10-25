f140f211d704be42923961f862ff973ffa70b0d9
nmaeda <nmaeda@keepdata.asia>
2019-10-26 01:12:12 +0900

    handle focus window to paste clipboard text; try auto encode or decode;

--- index.js
+++ index.js
@@ -10,18 +10,24 @@ if (document.readyState != "loading") {
 
 function onReady() {
     addEventListeners()
-    pasteClipboardText()
+    pasteClipboardText(tryEncodeOrDecode)
 }
 
-function pasteClipboardText() {
-    navigator.clipboard && navigator.clipboard.readText().then(function(clipText) {
-        document.getElementById("input1").innerText = clipText
-    })
+function onFocusWindow() {
+    pasteClipboardText(tryEncodeOrDecode)
 }
 
 function addEventListeners() {
     document.getElementById("encode-button").addEventListener("click", encode)
     document.getElementById("decode-button").addEventListener("click", decode)
+    window.addEventListener("focus", onFocusWindow)
+}
+
+function pasteClipboardText(onDone) {
+    navigator.clipboard && navigator.clipboard.readText().then(function(clipText) {
+        document.getElementById("input1").value = clipText
+        onDone && onDone()
+    })
 }
 
 function encode() {
@@ -33,5 +39,16 @@ function encode() {
 function decode() {
     let input = document.getElementById("input1").value
     let output = decodeURIComponent(input)
+    document.getElementById("output1").value = output
+}
+
+function tryEncodeOrDecode() {
+    let input = document.getElementById("input1").value
+    let output = decodeURIComponent(input)
+
+    if (input === output) {
+        output = encodeURIComponent(input)
+    }
+
     document.getElementById("output1").value = output
 }
